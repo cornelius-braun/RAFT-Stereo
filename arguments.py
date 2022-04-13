aaa0939ce120d7c2499489161711be1ae414821b
Cornelius <cornelius.braun@posteo.de>
2022-04-13 16:53:04 +0100

    eval

--- arguments.py
+++ arguments.py
@@ -1,6 +1,6 @@
 class Args:
     # model_args;
-    restore_ckpt = None
+    restore_ckpt = "checkpoints/100000_raft-stereo.pth"
     dataset="scared"
     mixed_precision = False
     valid_iters=32
@@ -9,7 +9,7 @@ class Args:
     corr_implementation = "reg"
     corr_levels = 4
     corr_radius = 4
-    n_downsample = 2
+    n_downsample = 3
     n_gru_layers = 3
     shared_backbone = False
     slow_fast_gru = False
